b6753a6df6b9beb1f4a5add53b4088e26843bf90
Livsy90 <artmir@live.ru>
2024-01-24 21:49:43 +0400

    Initial Commit

---
+++ OldTV/OldTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldTVApp.swift
+//  OldTV
+//
+//  Created by Livsy on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct OldTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
