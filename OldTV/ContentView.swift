5689e724b52f94f3dd70d663a5c87f267c544f6c
Livsy90 <artmir@live.ru>
2024-01-25 12:51:42 +0400

    Add New Images

--- OldTV/ContentView.swift
+++ OldTV/ContentView.swift
@@ -17,10 +17,10 @@ struct ContentView: View {
             GeometryReader { geo in
                 ZStack {
                     TimelineView(.animation) { _ in
-                        Image("street")
+                        Image("tokyo")
                             .resizable()
                             .scaledToFit()
-                            .blur(radius: 1.2)
+                            .blur(radius: 0.5)
                             .layerEffect(
                                 ShaderLibrary.oldTVShader(
                                     .float(-startDate.timeIntervalSinceNow),
@@ -31,28 +31,27 @@ struct ContentView: View {
                             .opacity(0.4)
                         
                     }
+                    
+                    Image("old-tv")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.bottom, -66)
                 }
                 .overlay(
-                    ZStack {
-                        Image("old-tv")
-                            .resizable()
-                            .scaledToFill()
-                        
-                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-                            .font(.custom("VT323-Regular", size: 20))
-                            .foregroundStyle(.white)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                            .padding(.vertical, 44)
-                            .padding(.leading, 55)
-                            .padding(.trailing, 150)
-                            .layerEffect(
-                                ShaderLibrary.oldTVShader(
-                                    .float(-startDate.timeIntervalSinceNow),
-                                    .float2(geo.size)
-                                ),
-                                maxSampleOffset: .zero
-                            )
-                    }
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                        .font(.custom("VT323-Regular", size: 20))
+                        .foregroundStyle(Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)))
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                        .padding(.vertical, 44)
+                        .padding(.leading, 65)
+                        .padding(.trailing, 150)
+                        .layerEffect(
+                            ShaderLibrary.oldTVShader(
+                                .float(-startDate.timeIntervalSinceNow),
+                                .float2(geo.size)
+                            ),
+                            maxSampleOffset: .zero
+                        )
                 )
             }
             .aspectRatio(1, contentMode: .fit)
